268169d58d4e538a61ebee912789682ffff43283
Gandarlpu <49300186+Gandarlpu@users.noreply.github.com>
2020-08-26 16:41:23 +0900

    Create Addview.kt

---
+++ Addview.kt
@@ -0,0 +1,42 @@
+package com.example.kolin_base
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
+import org.w3c.dom.Text
+import java.util.ArrayList
+
+class Addview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addview)
+
+
+        // 아이템 리스트 준비
+        val carList = ArrayList<CarForList>()
+        for (i in 0 until 10){
+            carList.add(CarForList("" + i + "번째 자동차" , "" + i + "순위 엔진"))
+        }
+
+
+        val container = findViewById<LinearLayout>(R.id.addview_container)
+        val inflater = LayoutInflater.from(this@Addview)
+        for(i in 0 until carList.size){
+            val itemView = inflater.inflate(R.layout.item_view , null)
+            val carNameView = itemView.findViewById<TextView>(R.id.car_name)
+            val carEngineView = itemView.findViewById<TextView>(R.id.car_engine)
+
+            carNameView.setText(carList.get(i).name)
+            carEngineView.setText(carList.get(i).engine)
+            container.addView(itemView)
+        }
+    }
+}
+
+
+
+class CarForList(val name : String , val engine : String){
+
+}
