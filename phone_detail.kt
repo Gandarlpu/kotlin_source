3095f3d763ad2e96a3024fec52818739bc41d80b
Gandarlpu <49300186+Gandarlpu@users.noreply.github.com>
2020-08-26 16:41:27 +0900

    Create phone_detail.kt

---
+++ phone_detail.kt
@@ -0,0 +1,11 @@
+package com.example.kolin_base
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class phone_detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_detail)
+    }
+}
