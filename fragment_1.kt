ec6294aa5e7790f203097bcc4fde153ec0f2366b
Gandarlpu <49300186+Gandarlpu@users.noreply.github.com>
2020-08-27 14:06:33 +0900

    Create fragment_1.kt

---
+++ fragment_1.kt
@@ -0,0 +1,16 @@
+package com.example.kolin_base
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class fragment_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_1)
+
+
+
+
+
+    }
+}
