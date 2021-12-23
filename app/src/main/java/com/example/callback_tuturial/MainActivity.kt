0998f4b5813a27afa01870a7a7092fec32b0eedd
Chance <phs811@naver.com>
2021-12-23 19:20:50 +0900

    layout

--- app/src/main/java/com/example/callback_tuturial/MainActivity.kt
+++ app/src/main/java/com/example/callback_tuturial/MainActivity.kt
@@ -4,33 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "로그"
 
+    // 뷰가 생성되었을 때
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 그릴 xml 뷰 파일을 연결 시켜준다. 즉, 설정한다.
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG , "MainActivity-onCreate() called")
 
-        someCallBackMethod(completion = {
-            Log.d(TAG , "MainActivitiy - 컴플레이션 블럭 호출됨 / it : $it")
-        })
 
     }
-
-    private fun someCallBackMethod(completion : (String) -> Unit) {
-        Log.d(TAG , "MainActivity - someCallbackMethod() called")
-
-        Handler().postDelayed({
-
-            completion("하하하 끝났다.")
-
-        } , 1000L)
-
-    }
-
 
 }
